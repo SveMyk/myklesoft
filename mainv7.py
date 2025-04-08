3d1e9f7738f6417839267feae71457660247ca38
SveMyk <myklesoft@hotmail.com>
2025-04-08 22:01:23 +0200

    Update mainv7.py

--- mainv7.py
+++ mainv7.py
@@ -124,9 +124,9 @@ def read_serial_from_arduino():
 
 def beregn_batteriprosent(spenning):
     # For 4S Li-ion: 12.0V (tom) – 16.8V (full)
-    maks = 16.8
-    min = 12.0
-    prosent = (spenning - min) / (maks - min) * 100
+    spenning_max = 16.8
+    spenning_min = 12.0
+    prosent = (spenning - spenning_min) / (spenning_max - spenning_min) * 100
     return int(max(0, min(100, prosent)))
 
 def linjenavigasjon():
